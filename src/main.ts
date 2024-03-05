90535fae6586afa6e6dd8f94b5c32a391a8ad0f0
VladOvOl <v.ovsanik2002@gmail.com>
2024-03-05 19:39:46 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser' 
+//import * as cookieParser from 'cookie-parser' 
+var cookieParser = require('cookie-parser')
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
