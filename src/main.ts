4e4248e529b9d34dea8e0dc23e5283318c21f359
VladOvOl <v.ovsanik2002@gmail.com>
2024-03-05 20:57:07 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//import * as cookieParser from 'cookie-parser' 
-var cookieParser = require('cookie-parser')
+import * as cookieParser from 'cookie-parser' 
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.setGlobalPrefix('api')
-	//app.use(cookieParser())
+	app.use(cookieParser())
 	app.enableCors({
-		origin: ['http://localhost:5555'],
+		origin: 'http://localhost:5555/',
 		credentials: true,
 		exposedHeaders: 'set-cookie'
 	})
