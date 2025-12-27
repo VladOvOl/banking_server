1813d51bb8a779a08d0309d60f7dc1093eeac1e0
VladOvOl <v.ovsanik2002@gmail.com>
2025-12-27 18:28:22 +0100

    Add enable Cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function startServer() {
 	const app = await NestFactory.create(AppModule);
 
 	app.setGlobalPrefix('api')
-	app.use(cookieParser())
+
 	app.enableCors({
 		origin: [
       'http://localhost:5555',
@@ -18,25 +18,7 @@ async function startServer() {
     exposedHeaders: ['set-cookie']
 	})
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5555');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, PATCH, DELETE, OPTIONS'
-    );
-
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-
-    next();
-  });
-
+  app.use(cookieParser())
   await app.listen(7777);
 }
 
